4df7b635b7b6e60428f9074f1dc93f41b319422b
DoYeon511 <emily0511@naver.com>
2026-06-24 13:47:02 +0900

    주석 추가

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
+
+# git gitsync 확인을 위한 주석
 default_args = {
     'owner': 'doyeon',
     'depends_on_past': False,
